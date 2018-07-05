60420833d7e7bb13fbc1a9bdc12895aa7230171a
cliftonavil <cliftonavil@gmail.com>
2018-07-05 18:25:45 +0530

    added mobile & email fields

--- labproject/urls.py
+++ labproject/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/',admin.site.urls),
     url('', include('lab.urls')),
+    url('accounts/', include('accounts.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
 ]
 #1
