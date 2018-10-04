bb73037270363f6db3b058dce30f9d9fe19fe831
Clifton Avil D'Souza <cliftonavil@gmail.com>
2018-10-04 16:56:21 +0530

    Delete task.py

--- labproject/task.py
+++
@@ -1,9 +0,0 @@
-import re
-change_with = 'clifton'
-reverse_list = []
-for i in change_with:
-    reverse_list.insert(0, i)
-    y=" ".join(reverse_list)
-    reversed_final=re.sub(' ', '', y)
-print(reversed_final)
-
